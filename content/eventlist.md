e00010ec3817bcdee664bb949e4fa1f3d2e498f5
terminal98 <6383265+terminal98@users.noreply.github.com>
2024-10-20 02:59:16 +0900

    予定表をカレンダーにした！！

--- content/eventlist.md
+++ content/eventlist.md
@@ -1,5 +1,5 @@
 ---
-title: "依頼受付状況・イベント参加予定"
+title: "スケジュール"
 date: 2023-03-26T02:36:15+09:00
 draft: false
 series: ["撮影について"]
@@ -7,27 +7,25 @@ series_order: 2
 layout: "simple"
 ---
 
-## 依頼受付状況 {#request-status}
+<h3 class="profile-name text-center text-2xl">依頼受付カレンダー</h3>
 
-- 土休日: 前日まで受付中 
-- 平　日: 1ヶ月前まで受付中
+<div id="cal-button">
+<button id="prevButton" class="m-1 rounded enabled:bg-neutral-300 p-1.5 enabled:text-neutral-700 enabled:hover:bg-primary-500 enabled:hover:text-neutral enabled:dark:bg-neutral-700 dark:text-neutral-300 enabled:dark:hover:bg-primary-400 enabled:dark:hover:text-neutral-800 disabled:border disabled:border-neutral-300 disabled:dark:border-neutral-700 disabled:cursor-not-allowed disabled:m-0.5" onclick="changeMonth(-1)">前月</button>
+<span id="monthYear" class="p-1.5"></span>
+<button id="nextButton" class="m-1 rounded enabled:bg-neutral-300 p-1.5 enabled:text-neutral-700 enabled:hover:bg-primary-500 enabled:hover:text-neutral enabled:dark:bg-neutral-700 dark:text-neutral-300 enabled:dark:hover:bg-primary-400 enabled:dark:hover:text-neutral-800 disabled:border disabled:border-neutral-300 disabled:dark:border-neutral-700 disabled:cursor-not-allowed disabled:m-0.5" onclick="changeMonth(1)">翌月</button>
+</div>
 
+<div id="calendar"></div>
 
-## 確定している予定 {#schedule-list}
+`◯ 受付中` `△ 要相談` `✕ 締切` `- 終了` `■ ｲﾍﾞﾝﾄ参加日`  
 
-- 下記に記載のない進行中の案件も多数ございます。ご相談はお早めにお願いいたします
-- 非公開案件は予定表へ記載しておりません
+<span id="showMaxRange">読込中です…</span>  
 
-| 年月       | 日                 |
-| ---------- | ------------------ |
-| 2024年10月 | 20, 23, 25, 26     |
-| 2024年11月 | 4, 27              |
-| 2024年12月 | 25, 28, 29, 30, 31 |
-| 2025年 1月 | 1, 2, 12, 22       |
+<script src="/show-event-cal.js"></script>
 
----
+記載のない進行中の案件も多数ございます。ご相談はお早めにお願いいたします。
 
-## イベント 参加予定 {#event}
+<h3 id="event" class="profile-name text-center text-2xl">イベント参加予定表</h3>
 
 ### 2024年 {#event-2024}
 
@@ -46,3 +44,4 @@ layout: "simple"
 | -------- | ----------------------- |
 | 4/26(土) | ※ニコニコ超会議2025(仮) |
 | 4/27(日) | ※ニコニコ超会議2025(仮) |
+
---
+++ docs/show-event-cal.js
@@ -0,0 +1,145 @@
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
+const nowDate = new Date().getDate();
+
+let maxMonthRange = 3; // 今月から3ヶ月分
+if (nowDate >= 25) {
+  maxMonthRange++; // 25日以降は1ヶ月増やす
+}
+
+let events = {};
+let holidays = {};
+
+function fetchData() {
+  return Promise.all([
+    fetch('/data/events.json').then(response => response.json()),
+    fetch('/data/holidays.json').then(response => response.json())
+  ]).then(data => {
+    events = data[0];
+    holidays = data[1];
+  }).catch(error => {
+    console.error('Error fetching data:', error);
+  });
+}
+
+function maxYearMonthGen() {
+  const threeMonthsLater = new Date(new Date().setMonth(new Date().getMonth() + maxMonthRange));
+  // 年と月を取得
+  const threeMonthsLaterYear = threeMonthsLater.getFullYear();
+  const threeMonthsLaterMonth = threeMonthsLater.getMonth() + 1; // 0から始まるので+1
+  document.getElementById('showMaxRange').innerText = `${threeMonthsLaterYear}年${threeMonthsLaterMonth}月までの予定を公開しています`;
+}
+
+function generateCalendar(month, year) {
+  const today = new Date();
+  let firstDay = new Date(year, month, 1).getDay();
+  let daysInMonth = new Date(year, month + 1, 0).getDate();
+  let calendarDiv = document.getElementById('calendar');
+  let calendarHtml = `<table><thead><tr><th class="mon">月</th><th class="tue">火</th><th class="wed">水</th><th class="thu">木</th><th class="fri">金</th><th class="sat">土</th><th class="sun">日</th></tr><tr></thead><tbody>`;
+
+  //document.getElementById('maxYearMonth').innerText = monthYearText;
+
+  const shiftDays = (firstDay === 0) ? 6 : firstDay - 1;
+  for (let i = 0; i < shiftDays; i++) {
+    calendarHtml += `<td class="day blank"></td>`;
+  }
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    if ((shiftDays + day - 1) % 7 === 0 && day !== 1) {
+      calendarHtml += `</tr><tr>`;
+    }
+    let dateStr = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+    let eventName = eventUrl = "";
+    if (events[dateStr]) {
+      eventName = events[dateStr].eventname;
+      if (events[dateStr].url) {
+        eventName = `<a href="${events[dateStr].url}" target="_blank">${eventName}</a>`;
+      }
+    }
+    let holiday = holidays[dateStr] ? holidays[dateStr] : '';
+    // 振替休日の処理
+    //if (holiday.includes('振替休日')) {
+    //  holiday = '振替休日';
+    //}
+
+    if (!eventName) {
+      const currentDate = new Date(year, month, day);
+      const daysDiff = Math.ceil((currentDate - today) / (1000 * 60 * 60 * 24));
+      const isWeekend = currentDate.getDay() === 0 || currentDate.getDay() === 6;
+
+      if (isWeekend || holiday) {
+        eventName = daysDiff === 0 ? '△' : '◯';
+      } else {
+        if (daysDiff < 0) {
+          eventName = daysDiff >= -7 ? '✕' : '△';
+        } else {
+          eventName = daysDiff <= 7 ? '-' : '△';
+        }
+      }
+    }
+
+    // 今日以前のイベントにバツ印を表示
+    let eventDisplay = new Date(dateStr) < today ? '-' : eventName;
+
+    let isToday = today.getMonth() === currentMonth && today.getDate() === day ? "today" : "";
+
+    if (holiday || (shiftDays + day - 1) % 7 == 6) {
+      calendarHtml += `<td class="day ${isToday}"><span class="num sun" title="${holiday}">${day}</span><span class="event-title">${eventDisplay}</span></td>`;
+    } else if ((shiftDays + day - 1) % 7 == 5) {
+      calendarHtml += `<td class="day ${isToday}"><span class="num sat">${day}</span><span class="event-title">${eventDisplay}</span></td>`;
+    } else {
+      calendarHtml += `<td class="day ${isToday}"><span class="num">${day}</span><span class="event-title">${eventDisplay}</span></td>`;
+    }
+  }
+
+  while ((shiftDays + daysInMonth) % 7 !== 0) {
+    calendarHtml += `<td></td>`;
+    daysInMonth++;
+  }
+
+  calendarHtml += `</tr><tbody></table>`;
+  calendarDiv.innerHTML = calendarHtml;
+
+  let monthYearText = `${year}年${month + 1}月`;
+  document.getElementById('monthYear').innerText = monthYearText;
+
+  // 月切替ボタンの制御
+  const prevButton = document.getElementById('prevButton');
+  const nextButton = document.getElementById('nextButton');
+
+  if (month === new Date().getMonth()) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+  }
+
+  if ((month === new Date().getMonth() + maxMonthRange || (month + 12) === new Date().getMonth() + maxMonthRange)) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false
+  }
+
+}
+
+function updateCalendar() {
+  generateCalendar(currentMonth, currentYear);
+}
+
+function changeMonth(change) {
+  currentMonth += change;
+  if (currentMonth < 0) {
+    currentMonth = 11;
+    currentYear--;
+  } else if (currentMonth > 11) {
+    currentMonth = 0;
+    currentYear++;
+  }
+  updateCalendar();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData().then(() => {
+    updateCalendar();
+  });
+  maxYearMonthGen();
+});
