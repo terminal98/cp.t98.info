dbeb1b4436362ecaaa1864383c99c3cd2c4dcb90
terminal98 <6383265+terminal98@users.noreply.github.com>
2025-08-13 19:34:42 +0900

    update

--- docs/eventlist/script.js
+++ docs/eventlist/script.js
@@ -269,7 +269,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const paginatedItems = eventList.slice(startIndex, endIndex);
 
     const table = document.createElement('table');
-    table.className = 'w-full text-sm text-left text-gray-500 dark:text-gray-400';
+    table.className = 'w-full text-sm text-left text-gray-500 dark:text-gray-400 mt-0 mb-0';
     table.innerHTML = `
             <thead class="text-xs text-gray-700 dark:text-gray-400 uppercase bg-gray-50 dark:bg-gray-700">
                 <tr>
